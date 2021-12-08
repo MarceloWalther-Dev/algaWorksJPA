a60165c47cfd18a143f5c1b7984e3c25d838b71d
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-12-08 19:50:59 -0300

    @TableGenerator

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -6,8 +6,13 @@ import javax.persistence.*;
 @Table(name = "categoria")
 public class Categoria {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+    @TableGenerator(name = "tabela", table = "hibernate_sequences",
+                         pkColumnName = "sequence_name",
+                         pkColumnValue = "categoria",
+                         valueColumnName = "next_val",
+                         initialValue = 0,
+                         allocationSize = 50)
     private Integer id;
 
     private String nome;
