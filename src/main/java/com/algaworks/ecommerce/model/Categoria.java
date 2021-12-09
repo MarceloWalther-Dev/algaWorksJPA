77a50e002fbdbbd775b0892e4fc4035b3259b0d3
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-12-09 18:27:59 -0300

    categoria se auto mapeia

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "categoria")
@@ -11,8 +12,13 @@ public class Categoria {
 
     private String nome;
 
-    @Column(name = "categoria_pai_id")
-    private Integer categoriaPaiId;
+    @ManyToOne
+    @JoinColumn(name = "categoria_pai_id")
+    private Categoria categoriaPai;
+
+    @OneToMany(mappedBy = "categoriaPai")
+    private List<Categoria> categorias;
+
 
     public Integer getId() {
         return id;
@@ -30,12 +36,12 @@ public class Categoria {
         this.nome = nome;
     }
 
-    public Integer getCategoriaPaiId() {
-        return categoriaPaiId;
+    public Categoria getCategoriaPai() {
+        return categoriaPai;
     }
 
-    public void setCategoriaPaiId(Integer categoriaPaiId) {
-        this.categoriaPaiId = categoriaPaiId;
+    public void setCategoriaPai(Categoria categoriaPai) {
+        this.categoriaPai = categoriaPai;
     }
 
     @Override
