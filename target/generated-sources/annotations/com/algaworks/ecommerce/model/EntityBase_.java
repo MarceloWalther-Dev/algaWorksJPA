17544acd5fe7d4e539bbc0344adf42da71596da1
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2022-04-08 07:30:57 -0300

    10.13-tipagem-forte-com-metamodel

---
+++ target/generated-sources/annotations/com/algaworks/ecommerce/model/EntityBase_.java
@@ -0,0 +1,16 @@
+package com.algaworks.ecommerce.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(EntityBase.class)
+public abstract class EntityBase_ {
+
+	public static volatile SingularAttribute<EntityBase, Integer> id;
+
+	public static final String ID = "id";
+
+}
+
