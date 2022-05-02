3d25185ef2d697a7cea33c981481421e7f59da77
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2022-05-02 08:31:17 -0300

    12.8. O problema do @OneToOne com o lazy no Hibernate

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -1,9 +1,14 @@
 package com.algaworks.ecommerce.model;
 
 import com.algaworks.ecommerce.listener.GerarNotaFiscalListener;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.engine.spi.PersistentAttributeInterceptable;
+import org.hibernate.engine.spi.PersistentAttributeInterceptor;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
@@ -17,17 +22,20 @@ import java.util.List;
 @Entity
 @Table(name = "pedido")
 @EntityListeners({GerarNotaFiscalListener.class})
-public class Pedido extends EntityBase{
+public class Pedido extends EntityBase {
+        //implements PersistentAttributeInterceptable {
 
 
     @NotNull
-    @ManyToOne(optional = false)//Optional ele vai fazer um joinColumn em vez de fazer um left joinColumn, mais importante na hora de construir uma query
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+//Optional ele vai fazer um joinColumn em vez de fazer um left joinColumn, mais importante na hora de construir uma query
     @JoinColumn(name = "cliente_id", nullable = false,
-            foreignKey = @ForeignKey(name = "fk_pedido_cliente")) // fk_pedido_cliente = fk_pedido e onde vai ficar a foreign key e cliente e a referencia da tabela
+            foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
+    // fk_pedido_cliente = fk_pedido e onde vai ficar a foreign key e cliente e a referencia da tabela
     private Cliente cliente;
 
     @NotEmpty
-    @OneToMany(mappedBy = "pedido",cascade = CascadeType.MERGE)// cascade = CascadeType.PERSIST
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.MERGE)// cascade = CascadeType.PERSIST
     private List<ItemPedido> itensPedido;
 
     @PastOrPresent
@@ -42,8 +50,8 @@ public class Pedido extends EntityBase{
     @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
-
-    @OneToOne(mappedBy = "pedido")
+    //@LazyToOne(LazyToOneOption.NO_PROXY)
+    @OneToOne(mappedBy = "pedido", fetch = FetchType.LAZY)
     private NotaFiscal notaFiscal;
 
     @NotNull
@@ -60,20 +68,72 @@ public class Pedido extends EntityBase{
     @Column(name = "endereco_entrega")
     private EnderecoEntregaPedido enderecoEntrega;
 
-    @OneToOne(mappedBy = "pedido")
+    //@LazyToOne(LazyToOneOption.NO_PROXY)
+    @OneToOne(mappedBy = "pedido", fetch = FetchType.LAZY)
     private Pagamento pagamento;
 
-    public boolean isPago(){
+//    @Transient
+//    @Setter(AccessLevel.NONE)// utilizamos essa anotacao pq substituimos o setter pelo public void $$_hibernate_setInterceptor
+//    @Getter(AccessLevel.NONE)// utilizamos essa anotacao pq substituimos o getter pelo public PersistentAttributeInterceptor $$_hibernate_getInterceptor()
+//    private PersistentAttributeInterceptor persistentAttributeInterceptor;
+
+
+//    public NotaFiscal getNotaFiscal() {
+//        if (this.persistentAttributeInterceptor != null) {
+//            return (NotaFiscal) persistentAttributeInterceptor.readObject(this, "notaFiscal", this.notaFiscal);
+//        } else {
+//            return notaFiscal;
+//        }
+//    }
+//
+//    public void setNotaFiscal(NotaFiscal notaFiscal) {
+//        if (this.persistentAttributeInterceptor != null) {
+//           this.notaFiscal = (NotaFiscal) persistentAttributeInterceptor.writeObject(this, "notaFiscal", this.notaFiscal, notaFiscal);
+//        } else {
+//            this.notaFiscal = notaFiscal;
+//        }
+//    }
+//
+//    public Pagamento getPagamento() {
+//        if (this.persistentAttributeInterceptor != null) {
+//            return (Pagamento) persistentAttributeInterceptor.readObject(this, "pagamento", this.pagamento);
+//        } else {
+//            return pagamento;
+//        }
+//    }
+//
+//    public void setPagamento(Pagamento pagamento) {
+//        if (this.persistentAttributeInterceptor != null) {
+//            this.pagamento = (Pagamento)persistentAttributeInterceptor.writeObject(this, "pagamento", this.pagamento, pagamento);
+//        } else {
+//            this.pagamento = pagamento;
+//        }
+//    }
+
+
+//    // get do atributo persistentAttributeInterceptor
+//    @Override
+//    public PersistentAttributeInterceptor $$_hibernate_getInterceptor() {
+//        return this.persistentAttributeInterceptor;
+//    }
+//
+//    //set do atributo persistentAttributeInterceptor
+//    @Override
+//    public void $$_hibernate_setInterceptor(PersistentAttributeInterceptor persistentAttributeInterceptor) {
+//        this.persistentAttributeInterceptor = persistentAttributeInterceptor;
+//    }
+
+    public boolean isPago() {
         return StatusPedido.PAGO.equals(status);
     }
 
 
-    private void calcularTotal(){
-        if (this.itensPedido != null){
+    private void calcularTotal() {
+        if (this.itensPedido != null) {
             this.total = itensPedido.stream()
                     .map(i -> new BigDecimal(i.getQuantidade()).multiply(i.getPrecoProduto()))
                     .reduce(BigDecimal.ZERO, BigDecimal::add);
-        }else{
+        } else {
             total = BigDecimal.ZERO;
         }
     }
@@ -81,13 +141,13 @@ public class Pedido extends EntityBase{
 
     //callbacks{
     @PrePersist// a propriedade sera atribuida o valor assim que ele persistir
-    public void aoPersistir(){
-       this.dataCriacao = LocalDateTime.now();
-       this.calcularTotal();
+    public void aoPersistir() {
+        this.dataCriacao = LocalDateTime.now();
+        this.calcularTotal();
     }
 
     @PreUpdate// a propriedade recebera valor assim que acontecer o update
-    public void aoAtualizar(){
+    public void aoAtualizar() {
         this.dataUltimaAtualizacao = LocalDateTime.now();
         this.calcularTotal();
     }
@@ -116,4 +176,6 @@ public class Pedido extends EntityBase{
     public void aoCarregar() {
         System.out.println("Após carregar o Pedido.");
     }
+
+
 }
